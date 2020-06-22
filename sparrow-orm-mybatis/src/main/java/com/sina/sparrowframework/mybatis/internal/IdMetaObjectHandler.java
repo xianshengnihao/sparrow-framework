3954681bde8f96fcf10e2f74bc457b4fd57e51ce
songbo1 <songbo1@staff.sina.com.cn>
2020-06-22 21:58:11 +0800

    id

---
+++ sparrow-orm-mybatis/src/main/java/com/sina/sparrowframework/mybatis/internal/IdMetaObjectHandler.java
@@ -0,0 +1,23 @@
+package com.sina.sparrowframework.mybatis.internal;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.sina.sparrowframework.id.conf.IdProviderConfig;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+/**
+ * <p>id自动赋值</p>
+ */
+@Component("idMetaObjectHandler")
+public class IdMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.setFieldValByName("id", IdProviderConfig.id.genId(),metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
