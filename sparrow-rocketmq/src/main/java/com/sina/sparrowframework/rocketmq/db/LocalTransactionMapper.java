ce708ddefc46ed1516094b6972a04a993e6a4846
songbo1 <songbo1@staff.sina.com.cn>
2020-06-22 22:08:19 +0800

    id

--- sparrow-rocketmq/src/main/java/com/sina/sparrowframework/rocketmq/db/LocalTransactionMapper.java
+++ sparrow-rocketmq/src/main/java/com/sina/sparrowframework/rocketmq/db/LocalTransactionMapper.java
@@ -2,8 +2,9 @@ package com.sina.sparrowframework.rocketmq.db;
 
 import com.sina.sparrowframework.mybatis.internal.IBaseDao;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
-@Mapper
-//@Component
+//@Mapper
+@Component
 public interface LocalTransactionMapper extends IBaseDao<MessageTransaction> {
 }
