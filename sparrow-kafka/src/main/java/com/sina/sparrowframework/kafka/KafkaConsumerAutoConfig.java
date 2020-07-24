66e1e91b52c47789c3c85f43cee7d449050c999b
liubin12 <liubin12@staff.sina.com.cn>
2020-07-24 15:51:58 +0800

    添加 kafka 的自动化配置

--- sparrow-kafka/src/main/java/com/sina/sparrowframework/kafka/KafkaConsumerAutoConfig.java
+++ sparrow-kafka/src/main/java/com/sina/sparrowframework/kafka/KafkaConsumerAutoConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
@@ -40,10 +41,11 @@ public class KafkaConsumerAutoConfig {
      * 自动提交的 ContainerFactory, 默认的方式.
      */
     @Bean
+    @Primary
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
     kafkaListenerContainerFactory(KafkaProperties kafkaProperties,
                                   KafkaTemplate<String, String> kafkaTemplate) {
-        return buildKafkaListenerContainerFactory(kafkaProperties, kafkaTemplate, false);
+        return buildKafkaListenerContainerFactory(kafkaProperties, kafkaTemplate, Boolean.FALSE);
     }
 
     /**
@@ -55,7 +57,7 @@ public class KafkaConsumerAutoConfig {
     public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
     ackKafkaListenerContainerFactory(KafkaProperties kafkaProperties,
                                      KafkaTemplate<String, String> kafkaTemplate) {
-        return buildKafkaListenerContainerFactory(kafkaProperties, kafkaTemplate, true);
+        return buildKafkaListenerContainerFactory(kafkaProperties, kafkaTemplate, Boolean.TRUE);
     }
 
     private ConcurrentKafkaListenerContainerFactory<String, String>
@@ -64,7 +66,10 @@ public class KafkaConsumerAutoConfig {
         ConcurrentKafkaListenerContainerFactory<String, String> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
 
-        kafkaProperties.getConsumer().setEnableAutoCommit(!ack);
+        if (ack) {
+            kafkaProperties.getConsumer().setEnableAutoCommit(Boolean.FALSE);
+            kafkaProperties.getListener().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
+        }
 
         factory.setConsumerFactory(consumerFactory(kafkaProperties));
 
@@ -80,9 +85,6 @@ public class KafkaConsumerAutoConfig {
         map.from(() -> kafkaTemplate).whenNonNull().to(factory::setReplyTemplate);
         map.from(properties::getType).whenEqualTo(KafkaProperties.Listener.Type.BATCH)
                 .toCall(() -> factory.setBatchListener(true));
-        if (ack) {
-            factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-        }
         // 配置容器
         configureContainer(factory.getContainerProperties(), kafkaProperties);
         return factory;
@@ -90,26 +92,20 @@ public class KafkaConsumerAutoConfig {
 
 
     private void configureContainer(ContainerProperties container, KafkaProperties kafkaProperties) {
-        PropertyMapper map = PropertyMapper.get();
+        PropertyMapper map = PropertyMapper.get().alwaysApplyingWhenNonNull();
 
         KafkaProperties.Listener properties = kafkaProperties.getListener();
 
-        map.from(properties::getAckMode).whenNonNull().to(container::setAckMode);
-        map.from(properties::getClientId).whenNonNull().to(container::setClientId);
-        map.from(properties::getAckCount).whenNonNull().to(container::setAckCount);
-        map.from(properties::getAckTime).whenNonNull().as(Duration::toMillis)
-                .to(container::setAckTime);
+        map.from(properties::getAckMode).to(container::setAckMode);
+        map.from(properties::getClientId).to(container::setClientId);
+        map.from(properties::getAckCount).to(container::setAckCount);
+        map.from(properties::getAckTime).as(Duration::toMillis).to(container::setAckTime);
 
-        map.from(properties::getPollTimeout).whenNonNull().as(Duration::toMillis)
-                .to(container::setPollTimeout);
-        map.from(properties::getNoPollThreshold).whenNonNull()
-                .to(container::setNoPollThreshold);
-        map.from(properties::getIdleEventInterval).whenNonNull().as(Duration::toMillis)
-                .to(container::setIdleEventInterval);
-        map.from(properties::getMonitorInterval).whenNonNull().as(Duration::getSeconds)
-                .as(Number::intValue).to(container::setMonitorInterval);
+        map.from(properties::getPollTimeout).as(Duration::toMillis).to(container::setPollTimeout);
+        map.from(properties::getNoPollThreshold).to(container::setNoPollThreshold);
+        map.from(properties::getIdleEventInterval).as(Duration::toMillis).to(container::setIdleEventInterval);
+        map.from(properties::getMonitorInterval).as(Duration::getSeconds).as(Number::intValue).to(container::setMonitorInterval);
 
-        map.from(properties::getLogContainerConfig).whenNonNull()
-                .to(container::setLogContainerConfig);
+        map.from(properties::getLogContainerConfig).to(container::setLogContainerConfig);
     }
 }
