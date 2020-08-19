9c7757be25060c664edde85c8d4f251cbc7a00de
liubin12 <liubin12@staff.sina.com.cn>
2020-08-19 11:08:59 +0800

    删除无效的继承

--- sparrow-web/src/main/java/com/sina/sparrowframework/web/config/SparrowServletWebAutoConfig.java
+++ sparrow-web/src/main/java/com/sina/sparrowframework/web/config/SparrowServletWebAutoConfig.java
@@ -5,13 +5,8 @@ import com.sina.sparrowframework.tools.utility.JsonUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
 
 /**
  * created  on 2019-04-13.
@@ -19,18 +14,12 @@ import java.util.List;
 @ConditionalOnProperty(prefix = "sparrow.web", name = "enabled", havingValue = "true", matchIfMissing = true)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Configuration
-public class SparrowServletWebAutoConfig implements WebMvcConfigurer
-        , WebMvcRegistrations {
+public class SparrowServletWebAutoConfig {
 
     @Bean
     @ConditionalOnMissingBean(name = {"webObjectMapper"})
     public ObjectMapper webObjectMapper() {
         return JsonUtils.createNonNullObjectMapper();
-    }
-
-    @Override
-    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-
     }
 
 }
