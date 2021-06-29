116e34e9f01508f590f68c693591bd81105dd7bc
wangxiaonan <xiaonan11@staff.sina.com.cn>
2021-06-29 17:54:26 +0800

    提出扫码文件

--- sparrow-password/src/main/java/com/sina/sparrowframework/password/EnablePasswordConfiguration.java
+++ sparrow-password/src/main/java/com/sina/sparrowframework/password/EnablePasswordConfiguration.java
@@ -1,10 +1,10 @@
 package com.sina.sparrowframework.password;
 
+import com.sina.sparrowframework.password.conf.PasswordScanConf;
 import com.sina.sparrowframework.password.licai.AesArgumentConfig;
-import com.sina.sparrowframework.password.licai.HandlerMethodAesArgument;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -13,11 +13,8 @@ import org.springframework.context.annotation.Import;
  * @date 2021/6/9 4:56 下午
  */
 @Configuration
-@ComponentScan(value = {"com.sina.sparrowframework.password"})
-@ConditionalOnProperty(prefix = "sparrow.password", name = "enable", havingValue = "true", matchIfMissing = true)
-@Import({ AesArgumentConfig.class})
+@Import({ AesArgumentConfig.class, PasswordScanConf.class})
 public class EnablePasswordConfiguration {
-
 
 
 
---
+++ sparrow-password/src/main/java/com/sina/sparrowframework/password/conf/PasswordScanConf.java
@@ -0,0 +1,13 @@
+package com.sina.sparrowframework.password.conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author wxn
+ * @date 2021/6/29 4:30 下午
+ */
+@Configuration
+@ComponentScan(value = {"com.sina.sparrowframework.password"})
+public class PasswordScanConf {
+}
